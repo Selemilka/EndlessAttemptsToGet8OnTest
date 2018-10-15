e1ed94db8953806625667bf75acd11ab9bf603ec
Serega <sdfomin@edu.hse.ru>
2018-10-15 21:01:03 +0300

    Вариант 4

--- AnotherOneTinyAttempt/Program.cs
+++ AnotherOneTinyAttempt/Program.cs
@@ -8,17 +8,7 @@ namespace AnotherOneTinyAttempt
 {
     class Program
     {
-        /// <summary>
-        ///     Минимальный и максимальный размер массива (допустимый ввод)
-        /// </summary>
-        const int MinCorrectValue = 1, MaxCorrectValue = 1000000;
-
-        /// <summary>
-        ///     Метод, считвыающий целое число
-        /// </summary>
-        /// <param name="message">Сообщение, выводящееся на экран перед вводом</param>
-        /// <returns>Считанное число</returns>
-        public static int ReadInt(string message)
+        public static int ReadInt(string message, int minCorrectValue, int maxCorrectValue)
         {
             int result;
             bool isCorrect = false;
@@ -26,7 +16,7 @@ namespace AnotherOneTinyAttempt
             {
                 Console.Write(message);
                 if (int.TryParse(Console.ReadLine(), out result) &&
-                    (result >= MinCorrectValue && result <= MaxCorrectValue))
+                    (result >= minCorrectValue && result <= maxCorrectValue))
                     isCorrect = true;
                 else
                     Console.WriteLine("Неверный формат ввода!!");
@@ -35,12 +25,44 @@ namespace AnotherOneTinyAttempt
             return result;
         }
 
+        public static void SummCalculate(int n, out double s1, out double s2)
+        {
+            double[] r = new double[n + 1];
+            r[0] = 1;
+            for (int i = 1; i <= n; ++i)
+            {
+                double sum = 0;
+                double fraction = i + 1; //Значение дроби при k = 0
+                for (int k = 1; k <= i; ++k)
+                {
+                    fraction = fraction / Math.Max((i - k), 1) * (k + 1);
+                    sum += fraction * r[i - k];
+                }
+                r[i] = -1d / (n + 1) * sum;
+            }
+            s1 = 0;
+            s2 = 0;
+            for (int i = 0; i <= n; i += 2)
+                s1 += r[i];
+            for (int i = 1; i <= n; i += 2)
+                s2 += r[i];
+        }
+
+        public static void Print(int a, double b, double c)
+        {
+            Console.WriteLine($"{a} {b:F3} {c:F3}");
+        }
 
         static void Main(string[] args)
         {
             do
             {
-
+                int n = ReadInt("Введите n: ", 2, 1000);
+                for (int i = 1; i < n; ++i)
+                {
+                    SummCalculate(i, out double s1, out double s2);
+                    Print(i, s1, s2);
+                }
             } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
     }
